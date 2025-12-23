d36b85b57eab619cec370dfca66c760132ebcee7
manasjiw26 <manasjiw26@gmail.com>
2025-12-23 20:49:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum indexed character/minimum-indexed-character.cpp
+++ Difficulty: Easy/Minimum indexed character/minimum-indexed-character.cpp
@@ -1,22 +1,23 @@
 class Solution {
   public:
-    int minIndexChar(string &s1, string &s2) {
-        //  code here
+    string printMinIndexChar(string S, string patt) {
+        // Code here
         const int CHAR = 256;
         int arr[CHAR];
         fill(arr,arr+CHAR,-1);
-        for(int i=0;i<s1.length();i++){
-            if(arr[s1[i]]==-1){
-                arr[s1[i]] = i;
+        for(int i=0;i<S.length();i++){
+            if(arr[S[i]]==-1){
+                arr[S[i]] = i;
             }
         }
         int res = INT_MAX;
-        for(int j = 0;j<s2.length();j++){
-            if(arr[s2[j]]>=0){
-            res = min(res,arr[s2[j]]);}
+        for(int j = 0;j<patt.length();j++){
+            if(arr[patt[j]]>=0){
+            res = min(res,arr[patt[j]]);}
         }
-        return (res != INT_MAX)? res : -1;
-        
+        string result;
+        result += (res != INT_MAX)? S[res] : '$';
+        return result;
     }
     int min(int a,int b){
         if(a<=b){
