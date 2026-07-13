84edfebcc9d34fd3b8212109d1353008efeee619
manasjiw26 <manasjiw26@gmail.com>
2026-07-13 15:18:35 +0530

    Time: 1536 ms (5.23%) | Memory: 46.9 MB (5.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-    vector<int> prefix,suffix;
-    int preprod = 1, sufprod = 1;
-    for(int i = 0;i<nums.size();i++){
-        preprod = preprod*nums[i];
-        prefix.push_back(preprod);
-        sufprod = sufprod*nums[n-i-1];
-        suffix.push_back(sufprod);
-    }
-    reverse(suffix.begin(),suffix.end());
-    vector<int> res;
-    res.push_back(suffix[1]);
-    for(int i = 1;i<nums.size()-1;i++){
-        res.push_back(prefix[i-1]*suffix[i+1]);
-    }
-    res.push_back(prefix[n-2]);
-    return res;
+        if(n <2){
+            return {1};
+        }
+        vector<int> leftmul,rightmul;
+        leftmul.push_back(1);
+        rightmul.insert(rightmul.begin(),1);
+        for(int i = 1;i<nums.size();i++){
+            
+                leftmul.push_back(leftmul.back()*nums[i-1]);
+                rightmul.insert(rightmul.begin(),rightmul.front()*nums[n-i]);
+            
+        }
+
+        for(int i=0;i<n;i++){
+            cout << leftmul[i];
+        }
+        cout<<endl;
+        for(int i=0;i<n;i++){
+            cout << rightmul[i];
+        }
+        
+        vector<int> res;
+        res.push_back(rightmul[0]);
+        for(int i = 1;i<n-1;i++){
+            res.push_back(leftmul[i]*rightmul[i]);
+        }
+        res.push_back(leftmul[n-1]);
+        return res;
     }
 };
